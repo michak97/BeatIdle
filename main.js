b37067c4eaa66bc2ee6c7aaac08ec673cfe2b16e
michak97 <102417597+mk1307@users.noreply.github.com>
2023-02-10 11:23:20 +0100

    FEAT: new gameLoop class

--- main.js
+++ main.js
@@ -1,47 +1,21 @@
-function loadTime(){
-  const time = window.localStorage.getItem("total_time");
-  if (time) return parseFloat(time);
-  return 0;
-}
+const loop = new GameLoop();
 
-function saveTime(time){
-  window.localStorage.setItem("total_time", time);
-}
+let currency = 0;
+let currency_per_millisecond = 0.003;
 
-let last_time=null;
-let total_time=loadTime();
+let currencyEl = document.querySelector('#currency');
 
-window.addEventListener("beforeunload", () => saveTime(total_time));
+loop.onUpdate = function(dt, t) {
+  currency += currency_per_millisecond * dt;
+};
 
-setInterval(function gameLoop() {
-  const current_time = performance.now();
+loop.onRender = function(i) {
+  currencyEl.textContent = currency.toFixed(2);
+};
 
-  if (last_time === null){
-    last_time = current_time;
-  }
+loop.onPanic = function() {
+  // discard any accumulated lag time and hope for the best
+  this.timing.lag = 0;
+};
 
-  const delta_time = current_time - last_time;
-  total_time += delta_time;
-  last_time = current_time;
-
-  updateGame(delta_time, total_time)
-}, 1000/60);
-
-let building_a_currency_per_millisecond = 0.001;
-let building_b_currency_per_millisecond = 0.003;
-
-let currency = (building_a_currency_per_millisecond * total_time) + (building_b_currency_per_millisecond * total_time);
-
-let last_save = 0;
-
-function updateGame(delta_time, total_time){
-  if (total_time-last_save >= 5000){
-    saveTime(total_time);
-    last_save=total_time;
-  }
-  currency += (building_a_currency_per_millisecond) * delta_time;
-  currency += (building_b_currency_per_millisecond) * delta_time;
-  currency_display = document.querySelector('#currency');
-
-  currency_display.textContent = currency.toFixed(2);
-}
+loop.start();
